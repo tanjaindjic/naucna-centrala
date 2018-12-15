bc883dc88419618bb055e4011503f055f3ed2d6b
tanjaindjic <mali.patuljko@gmail.com>
2018-12-15 15:55:30 +0100

    here we go

---
+++ naucna-centrala/src/main/java/master/naucnacentrala/NaucnaCentralaApplication.java
@@ -0,0 +1,14 @@
+package master.naucnacentrala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaucnaCentralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaucnaCentralaApplication.class, args);
+	}
+
+}
+
---
+++ naucna-centrala/src/test/java/master/naucnacentrala/NaucnaCentralaApplicationTests.java
@@ -0,0 +1,17 @@
+package master.naucnacentrala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NaucnaCentralaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
